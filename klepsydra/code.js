0c6b1154b3c531c54663355a9d92a56a5c084a5a
Joao Silva <Joao@abc.com>
2021-05-20 22:21:15 +0200

    Hourglass

--- klepsydra/code.js
+++ klepsydra/code.js
@@ -1,3 +1,24 @@
+counterSeconds = 0;
+
 startCounter = () => {
-    alert('xxx');
+
+    counterSeconds = 5;
+
+    updateCounterText();
+}
+
+updateCounterText = () => {
+
+    var counterTextDiv = document.getElementById("counterText");
+
+    counterTextDiv.innerHTML = counterSeconds;
+
+    counterSeconds = counterSeconds - 1;
+
+    if (counterSeconds >= 0) {
+        setTimeout(updateCounterText, 1000);
+    } else {
+        var audio = new Audio('clong-1.mp3');
+        audio.play();
+    }
 }
