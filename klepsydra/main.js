68946bfdea30ec1b136d8109c36984d2409b311c
Joao Silva <Joao@abc.com>
2021-05-28 22:06:44 +0200

    Get time from query string

--- klepsydra/main.js
+++ klepsydra/main.js
@@ -1,27 +1,55 @@
 counterSeconds = 0;
 
-startCounter = () => {
 
-    counterSeconds = 64;
+
+audioStart = new Audio('button-3.mp3');
+audioEnd = new Audio('clong-1.mp3');
+
+startCounter = () => {
     
-    var audio = new Audio('button-3.mp3');
-        audio.play();
+    audioStart.play();
 
     updateCounterText();
 }
 
-updateCounterText = () => {
+getInitialTime = () => {
+    
+    var counterFromQueryString = getParameterByName('czas');
+    
+    if(counterFromQueryString != null){
+        return parseInt(counterFromQueryString);    
+    }
+    
+    return 64;
+}
 
+setCounterText = x => {
     var counterTextDiv = document.getElementById("licznik");
 
-    counterTextDiv.innerHTML = counterSeconds;
+    counterTextDiv.innerHTML = x;
+}
+
+updateCounterText = () => {
+
+    setCounterText(counterSeconds);
 
     counterSeconds = counterSeconds - 1;
 
     if (counterSeconds >= 0) {
         setTimeout(updateCounterText, 1000);
     } else {
-        var audio = new Audio('clong-1.mp3');
-        audio.play();
+        audioEnd.play();
     }
 }
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+counterSeconds = getInitialTime();
+setCounterText(counterSeconds);
