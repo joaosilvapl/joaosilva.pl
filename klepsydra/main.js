a30e7fdd4e63b0ae77e0ccbee51655e592c46334
Joao Silva <Joao@abc.com>
2021-05-28 20:33:07 +0200

    Counter

---
+++ klepsydra/main.js
@@ -0,0 +1,24 @@
+counterSeconds = 0;
+
+startCounter = () => {
+
+    counterSeconds = 5;
+
+    updateCounterText();
+}
+
+updateCounterText = () => {
+
+    var counterTextDiv = document.getElementById("licznik");
+
+    counterTextDiv.innerHTML = counterSeconds;
+
+    counterSeconds = counterSeconds - 1;
+
+    if (counterSeconds >= 0) {
+        setTimeout(updateCounterText, 1000);
+    } else {
+        var audio = new Audio('clong-1.mp3');
+        audio.play();
+    }
+}
