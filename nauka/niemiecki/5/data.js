c436edd9e1e5aaef8726099d69aa6c406d1b38a0
Joao Silva <jhrecife@gmail.com>
2022-03-14 01:06:17 +0100

    nauka - moving files to common

---
+++ nauka/niemiecki/5/data.js
@@ -0,0 +1,29 @@
+allTasksData = [
+
+  {
+    title: "Rzeczowniki",
+    type: "table",
+
+    data: {
+      columnTitles: ["Niemiecki", "Polski"],
+      rowData: [
+        ["der Fuß", "stopa"],
+        ["die Nase", "nos"],
+        ["das Bein", "noga"],
+        ["Füße (Plural)", "stopy"],
+        ["Nasen (Plural)", "nosy"],
+        ["Beine (Plural)", "nogi"],
+        ["der Rücken", "plecy"],
+        ["die Hand", "dłoń"],
+        ["der Finger", "palec"],
+        ["der Kopf", "głowa"],
+        ["das Ohr", "ucho"],
+        ["der Bauch", "brzuch"],
+
+      ],
+    },
+  },
+];
+
+
+
--- nauka/niemiecki/common/code.js
+++ nauka/niemiecki/common/code.js
@@ -1,30 +1,3 @@
-var allTasksData = [
-
-  {
-    title: "Rzeczowniki",
-    type: "table",
-
-    data: {
-      columnTitles: ["Niemiecki", "Polski"],
-      rowData: [
-        ["der Fuß", "stopa"],
-        ["die Nase", "nos"],
-        ["das Bein", "noga"],
-        ["Füße (Plural)", "stopy"],
-        ["Nasen (Plural)", "nosy"],
-        ["Beine (Plural)", "nogi"],
-        ["der Rücken", "plecy"],
-        ["die Hand", "dłoń"],
-        ["der Finger", "palec"],
-        ["der Kopf", "głowa"],
-        ["das Ohr", "ucho"],
-        ["der Bauch", "brzuch"],
-
-      ],
-    },
-  },
-];
-
 taskElements = [];
 
 createAllTaskElements = () => {
