37baeba1d5a286f9e80e46b574162d2f0af2ff96
Joao Silva <jhrecife@gmail.com>
2022-03-17 23:20:49 +0100

    nauka - german 8

---
+++ nauka/niemiecki/8/data.js
@@ -0,0 +1,18 @@
+allTasksData = [
+  {
+    title: "Napisz zdania według wzoru.",
+    type: "table",
+
+    data: {
+      rowData: [
+        ["Manuela: Ich fühle mich schlecht.", "Manuela sagt, dass sie sich schlecht fühlt."],
+        ["Nina: Ich have Fieber.", "Nina sagt, dass sie Fieber hat."],
+        ["Peter: Ich gehe morgen zum Arzt.", "Peter sagt, dass er morgen zum Arzt geht."],
+        ["Katja: Ich habe einen Ausschlag bekommen.", "Katja sagt, dass sie einen Ausschlag bekommen hat."],
+        ["Philip: Ich habe Bauschmerzen und Durchfall.", "Philip sagt, dass er Bauschmerzen und Durchfall hat."],
+        ["Karl: Ich muss heute zu Hause bleiben.", "Karl sagt, dass er heute zu Hause bleiben muss."],
+      ],
+    },
+  },
+  
+];
