b0291b4af9c248a6d408a619f8b34c4bbb441bc8
Joao Silva <jhrecife@gmail.com>
2022-03-17 15:19:53 +0100

    nauka - german 1 - refactored

---
+++ nauka/niemiecki/1/data.js
@@ -0,0 +1,28 @@
+allTasksData = [
+  {
+  title: "Czasowniki nieregularne",
+  type: "table",
+
+  data: {
+    columnTitles: [
+      "Infinitiv",
+      "Präsens 3. Person",
+      "Präteritum",
+      "Partizip II",
+    ],
+    rowData: [
+      ["backen (piec)", "backt / bäckt", "backte / buk", "gebacken"],
+      ["befehlen (rozkazywać)", "befiehlt", "befahl", "befohlen"],
+      [" beginnen (zaczynać (się))","-","begann","begonnen"],
+      ["bieten (oferować)","-","bot","geboten"],
+      ["bitten (prosić)","-","bat","gebeten"],
+      ["bleiben (zostawać)","-","blieb","geblieben (s.)"],
+      ["nennen (nazywac)","-","nannte","genannt"],
+      ["raten (radzić","rät","riet","geraten"],
+      ["reiben (trzec)","-","rieb","gerieben"],
+      ["reißen (drzeć, rwać)","-","riss","gerissen"],
+      ["reiten (jeździć konno)","-","ritt","geritten (s.)"],
+      ["rennen (pędzić, biec)","-","rannte","gerannt (s.)"],
+    ],
+  },
+}];
