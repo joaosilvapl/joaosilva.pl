9182933798a868098dc62b9d40373991e38b9ca4
Joao Silva <jhrecife@gmail.com>
2022-03-17 15:42:04 +0100

    nauka - german 4 - refactor

---
+++ nauka/niemiecki/4/data.js
@@ -0,0 +1,49 @@
+var allTasksData = [
+
+  {
+    title: "Czas przeszły prosty - Präteritum",
+    type: "table",
+
+    data: {
+      columnTitles: ["Zaimek", "Czasownik"],
+      rowData: [
+        ["ich", "hatte"],
+        ["du", "hattest"],
+        ["er / sie / es", "hatte"],
+        ["wir", "hatten"],
+        ["ihr", "hattet"],
+        ["sie / Sie", "hatten"],
+
+      ],
+    },
+  },
+  {
+    title: "Spójnik dass w zdaniu",
+    type: "table",
+
+    data: {
+      columnTitles: ["Początek zdania", "czasownik"],
+      rowData: [
+        ["(Wiem, że on miał gorączkę) Ich weiß, dass er Fieber", "hatte"],
+        ["(Wiem, że on poszedł do lekarza) Ich weiß, dass er zum Arzt", "gegangen ist"],
+      ],
+    },
+  },
+
+  {
+    title: "Imperativ - tryb rozkazujący",
+    type: "table",
+
+    data: {
+      columnTitles: ["Infinitiv", "2. Person Singular", "Imperativ 2. Person Singular", "2. Person Plural", "Imperativ 2. Person Plural (ihr)"],
+      rowData: [
+        ["lesen (czytać)","du liest","Lies bitte!","ihr lest","Lest bitte!"],
+        ["zuhören (słuchać)","du hörst zu","Hör(e) mal zu!","ih hört zu","Hört mal zu!"],
+        ["sprechen (mówić)","du sprichst","Sprich mal!","ihr sprecht","Sprecht mal!"],
+        ["sich setzen (siadać)","du setzt dich","Setz(e) dich bitte!","ih setzt euch","Setzt euch bitte!"],
+        ["fahren (jechać)","du fährst","Fahr(e) mal!","ihr fahrt","Fahrt mal!"],
+        ["ruhig sein (być cicho)","du bist ruhig","Sei bitte ruhig!","ihr seid ruhig","Seid bitte ruhig!"],
+      ],
+    },
+  },
+];
