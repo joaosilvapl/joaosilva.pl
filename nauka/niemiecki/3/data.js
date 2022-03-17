a4a9b71a236ad642e2b8ac62e3da8818bb1ea28f
Joao Silva <jhrecife@gmail.com>
2022-03-17 15:36:00 +0100

    nauka - german 3 - refactor

---
+++ nauka/niemiecki/3/data.js
@@ -0,0 +1,67 @@
+var allTasksData = [
+
+  {
+    title: "Rzeczowniki",
+    type: "table",
+
+    data: {
+      columnTitles: ["Niemiecki", "Polski"],
+      rowData: [
+        ["(Maskulinum - rodzaj męski) der Fuß", "stopa"],
+        ["(Femininum - rodzaj żęnski) die Nase", "nos"],
+        ["(Neutrum - rodzaj nijaki) Bein", "noga"],
+        ["Füße", "stopy"],
+        ["Nasen", "nosy"],
+        ["Beine", "nogi"],
+
+      ],
+    },
+  },
+  {
+    title: "Nominativ (mianownik) - wer? was? (kto? co?)",
+    type: "table",
+
+    data: {
+      columnTitles: ["Określony", "Nieokreślony", "Przeczenie", "Zaimek dzierżawczy"],
+      rowData: [
+        ["der Fuß", "ein Fuß","kein Fuß","mein Fuß"],
+        ["die Nase", "eine Nase","keine Nase","meine Nase"],
+        ["das Bein", "ein Bein","kein Bein","mein Bein"],
+        ["(Plural) die Füße, Nasen, Beine", "Füße, Nasen, Beine","keine Füße, Nasen, Beine","meine Füße, Nasen, Beine"],
+
+      ],
+    },
+  },
+
+  {
+    title: "Dativ (celownik) - wem? (komu?)",
+    type: "table",
+
+    data: {
+      columnTitles: ["Określony", "Nieokreślony", "Przeczenie", "Zaimek dzierżawczy"],
+      rowData: [
+        ["dem Fuß", "einem Fuß","keinem Fuß","meinem Fuß"],
+        ["der Nase", "einer Nase","keiner Nase","meiner Nase"],
+        ["dem Bein", "einem Bein","keinem Bein","meinem Bein"],
+        ["den Füßen, Nasen, Beinen (Plural)", "Füßen, Nasen, Beinen","keinem Füßen, Nasen, Beinen","meinem Füßen, Nasen, Beinen"],
+
+      ],
+    },
+  },
+
+  {
+    title: "Akkusativ (biernik) - wen? was? (kogo? co?)",
+    type: "table",
+
+    data: {
+      columnTitles: ["Określony", "Nieokreślony", "Przeczenie", "Zaimek dzierżawczy"],
+      rowData: [
+        ["den Fuß", "einen Fuß","keinen Fuß","meinem Fuß"],
+        ["die Nase", "eine Nase","keine Nase","meine Nase"],
+        ["das Bein", "ein Bein","kein Bein","mein Bein"],
+        ["die Füße, Nasen, Beine (Plural)", "Füße, Nasen, Beine","keine Füße, Nasen, Beine","meine Füße, Nasen, Beine"],
+
+      ],
+    },
+  },
+];
